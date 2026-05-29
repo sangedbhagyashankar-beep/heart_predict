87c8937fd7e9e1000b84199367d77380614bcdc7
sangedbhagyashankar-beep <sangedbhagyashankar@gmail.com>
2026-05-29 18:52:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -363,4 +363,4 @@ def generate_referral(hospital_id):
 if __name__ == '__main__':
     with app.app_context(): 
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True) give updated app.py
